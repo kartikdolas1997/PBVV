abb193e6e08b9676a7010e61c60a14e8dee1b5d7
Kartik Dolas <kartikdolas1997@gmail.com>
2020-02-17 00:42:28 +0530

    Full UI is done except results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,11 @@ import {MatSelectModule} from '@angular/material/select';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import {ReactiveFormsModule } from '@angular/forms'
+import {MatInputModule} from '@angular/material/input';
+// import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +33,14 @@ import {MatButtonModule} from '@angular/material/button';
     MatSelectModule,
     MatFormFieldModule,
     MatButtonModule,
-    MatRadioModule
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    // MatTableModule, 
+    // MatPaginatorModule, 
+    // MatSortModule
+
     
   ],
 
--- src/app/components/eportal/eportal.component.ts
+++ src/app/components/eportal/eportal.component.ts
@@ -5,15 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './eportal.component.html',
   styleUrls: ['./eportal.component.css']
 })
+
 export class EportalComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {}
   
-    selected = 'option2';
-  
-    
-  
+    selected = 'none';
+
+      
+    favoriteheadboy: string;
+    headboys: string[] = ['Boy1', 'Boy2', 'Boy3', 'Boy4'];
+
+    favoriteheadgirl: string;
+    headgirls: string[] = ['Girl1', 'Girl2', 'Girl3', 'Girl4'];
 
 }
+
