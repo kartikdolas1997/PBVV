6e0ff48b2a962f6b18cf1782d2d78af92d32e84e
kartik <kartikdolas1997@gmail.com>
2020-03-15 22:33:38 +0530

    updated

--- src/app/components/eportal/eportal.component.ts
+++ src/app/components/eportal/eportal.component.ts
@@ -9,18 +9,64 @@ import {EportalService} from './eportal.service'
 
 export class EportalComponent implements OnInit {
 
-  constructor(private _eportalservice:EportalService) { }
+  constructor(private _eportalservice:EportalService
+    ) { }
 
   ngOnInit(): void {}
-  
+
     selected = 'none';
-
-      
     favoriteheadboy: string;
-    headboys = this._eportalservice.headboys;
-
     favoriteheadgirl: string;
-    headgirls = this._eportalservice.headgirls;
+      
+    headboys;
+    headgirls;
+  
+
+    
+    myFunction(){
+      // console.log("slect",test);
+      console.log("myFunction called")
+      if(this.selected =="Red")
+      {
+        this.headboys = this._eportalservice.headboys_red;
+        this.headgirls = this._eportalservice.headgirls_red;
+        console.log("Red");
+        
+      }
+
+      else if(this.selected =="Blue")
+      {
+        this.headboys = this._eportalservice.headboys_blue;
+        this.headgirls = this._eportalservice.headgirls_blue;
+      }
+
+      else if(this.selected =="Green")
+      {
+        this.headboys = this._eportalservice.headboys_green;
+        this.headgirls = this._eportalservice.headgirls_green;
+      }
+
+      else if(this.selected =="Yellow")
+      {
+        this.headboys = this._eportalservice.headboys_yellow;
+        this.headgirls = this._eportalservice.headgirls_yellow;
+      }
+      else 
+      {
+        this.headboys = this._eportalservice.headboys;
+        this.headgirls = this._eportalservice.headgirls;
+      }
+    }
+
+    onSubmit(e) {
+      console.log("submit");
+      var vote = {
+        house : e.value.house,
+        headboy : e.value.headboy, 
+        headgirl : e.value.headgirl
+
+      }
+      console.log(vote);
+    }
 
 }
-
--- src/app/components/eportal/eportal.service.ts
+++ src/app/components/eportal/eportal.service.ts
@@ -6,7 +6,19 @@ import { Injectable } from '@angular/core';
 export class EportalService {
 
   constructor() { }
+  headboys;
+  headgirls;
+  
+  headboys_red = ['Red_Boy 1', 'Red_Boy 2', 'Red_Boy 3', 'Red_Boy 4'];
+  headgirls_red = ['Red_Girl 1', 'Red_Girl 2', 'Red_Girl 3', 'Red_Girl 4'];
 
-  headboys = ['Boy 1', 'Boy 2', 'Boy 3', 'Boy 4'];
-  headgirls = ['Girl 1', 'Girl 2', 'Girl 3', 'Girl 4'];
+  headboys_yellow = ['Yellow_Boy 1', 'Yellow_Boy 2', 'Yellow_Boy 3', 'Yellow_Boy 4'];
+  headgirls_yellow = ['Yellow_Girl 1', 'Yellow_Girl 2', 'Yellow_Girl 3', 'Yellow_Girl 4'];
+
+  headboys_blue = ['Blue_Boy 1', 'Blue_Boy 2', 'Blue_Boy 3', 'Blue_Boy 4'];
+  headgirls_blue = ['Blue_Girl 1', 'Blue_Girl 2', 'Blue_Girl 3', 'Blue_Girl 4'];
+
+  headboys_green = ['Green_Boy 1', 'Green_Boy 2', 'Green_Boy 3', 'Green_Boy 4'];
+  headgirls_green = ['Green_Girl 1', 'Green_Girl 2', 'Green_Girl 3', 'Green_Girl 4'];
+
 }
